5bff27a89cc43ce048251d4fc3712e3f3c73448b
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-12 13:46:33 -0700

    axios with mongoDB

--- Instagram-Front/src/ImageUpload.js
+++ Instagram-Front/src/ImageUpload.js
@@ -3,7 +3,7 @@ import firebase from "firebase";
 import { storage, db } from "./firebase";
 import "./ImageUpload.css";
 import { Input, Button } from "@material-ui/core";
-
+import axios from './axios';
 
 
 const ImageUpload = ({ username }) => {
@@ -41,6 +41,12 @@ const ImageUpload = ({ username }) => {
           .getDownloadURL()
           .then((url) => {
             setUrl(url);
+
+            axios.post('upload', {
+              caption: caption, 
+              user: username, 
+              image: url
+            });
 
             // post image inside db
             db.collection("posts").add({
--- Instagram-Front/src/firebase.js
+++ Instagram-Front/src/firebase.js
@@ -1,14 +1,25 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBucH7Kzm8Ds5CbuUs8uQJlzTPh3eZcTzA",
-  authDomain: "instagram-clone-23884.firebaseapp.com",
-  databaseURL: "https://instagram-clone-23884.firebaseio.com",
-  projectId: "instagram-clone-23884",
-  storageBucket: "instagram-clone-23884.appspot.com",
-  messagingSenderId: "671034896143",
-  appId: "1:671034896143:web:3aceafdf2319c9f1fc587a",
+  // sonny data
+  // apiKey: "AIzaSyBucH7Kzm8Ds5CbuUs8uQJlzTPh3eZcTzA",
+  // authDomain: "instagram-clone-23884.firebaseapp.com",
+  // databaseURL: "https://instagram-clone-23884.firebaseio.com",
+  // projectId: "instagram-clone-23884",
+  // storageBucket: "instagram-clone-23884.appspot.com",
+  // messagingSenderId: "671034896143",
+  // appId: "1:671034896143:web:3aceafdf2319c9f1fc587a",
+    apiKey: "AIzaSyCQkJ3v2JHrC0W-7deJP1tVpMb8LFpskGY",
+    authDomain: "instagram-with-react-30fa4.firebaseapp.com",
+    databaseURL: "https://instagram-with-react-30fa4.firebaseio.com",
+    projectId: "instagram-with-react-30fa4",
+    storageBucket: "instagram-with-react-30fa4.appspot.com",
+    messagingSenderId: "816384839312",
+    appId: "1:816384839312:web:e972f196e4c55f65c0d215",
+    measurementId: "G-QYXLGZJ8J9"
 });
+
+
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
